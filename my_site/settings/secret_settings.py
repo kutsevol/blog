fc8e192d3b7ae54f761af5e0b00338eeab1b4aea
Artur Kutsevol <arthur.kutsevol@gmail.com>
2017-09-26 18:52:07 +0300

    Update gitignore

--- my_site/settings/secret_settings.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i^!9*x1oc#pg-_0z)2)53-%yzf2nz_%au*w$d*9b59un%zd=lx'
-
-MARTOR_IMGUR_CLIENT_ID = '5aef17567500e89'
-DRACEDITOR_IMGUR_API_KEY = '80cd76407fd0c6449dba0d0102c7d4edc068a533'
