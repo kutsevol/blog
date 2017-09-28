6e3c42c54712229603ec5ea21eb1fec6bc483c14
Artur Kutsevol <AKutsevol@luxoft.com>
2017-09-28 16:22:33 +0300

    Test commit with settings.prod

--- my_site/settings/prod.py
+++ my_site/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+DATABASES['default'] = dj_database_url.config()
