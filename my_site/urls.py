085e1d72789bbee4a8f3adb1a57158de34de8438
Artur Kutsevol <AKutsevol@luxoft.com>
2017-10-02 17:59:26 +0300

    Refactoring urlpatterns for admin in urls.py

--- my_site/urls.py
+++ my_site/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^prod-admin/', admin.site.urls),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^martor/', include('martor.urls')),
     url(r'', include('blog.urls'))
