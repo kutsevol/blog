674db6dd66f3b7c4402c68d8bf700beb38240357
Artur Kutsevol <arthur.kutsevol@gmail.com>
2017-10-09 18:04:37 +0300

    Change name from post_list to posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import PostListView, PostView, CategoryView, CategoryListView
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view(), name='post_list'),
+    url(r'^$', PostListView.as_view(), name='posts'),
     url(r'^(?P<slug>[-\w\d\_]+)/$', PostView.as_view(), name='post'),
     url(r'^categories/?$', CategoryListView.as_view(), name='categories'),
     url(r'^category/(?P<slug>[-\w\d\_]+)/$', CategoryView.as_view(), name='category')
