991ed37ff9c858da3b773d0d3c570054457d5384
Artur Kutsevol <arthur.kutsevol@gmail.com>
2017-10-09 18:05:44 +0300

    Add extra get_context_data in PostView, PostListView and CategoryListView

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from django.views.generic import DetailView, ListView
 from django.utils import timezone
+from django.db.models import Count
 from .models import Post, Category
 
+p_values = Post.objects.values
 
-class PostListView(ListView):
-    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+class AbstractPost:
+
+    def get_context_data(self, **kwargs):
+        context = super(AbstractPost, self).get_context_data(**kwargs)
+        value = ('title', 'category__title')
+        context['posts_category'] = p_values(*value)
+        return context
+
+
+class PostListView(ListView, AbstractPost):
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
 
-class PostView(DetailView):
+class PostView(DetailView, AbstractPost):
     model = Post
     template_name = 'blog/post.html'
     context_object_name = 'post'
@@ -18,6 +31,16 @@ class CategoryListView(ListView):
     queryset = Category.objects.all()
     template_name = 'blog/categories.html'
     context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+
+        value = 'category__title'
+        context['count_category'] = p_values(value).order_by('category').annotate(count=Count('category'))
+
+        value = ('title', 'slug', 'published_date', 'category__title')
+        context['post_category_date'] = p_values(*value).order_by('-published_date')
+        return context
 
 
 class CategoryView(DetailView):
