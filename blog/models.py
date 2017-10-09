b2814f8d545e7654cc3e2151d251149447a17cd7
Artur Kutsevol <arthur.kutsevol@gmail.com>
2017-10-09 18:03:07 +0300

    Change category from ManyToMany to ForeignKey. Change all links into categories

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ from martor.models import MartorField
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=200, unique=True, blank=False)
-    slug = models.SlugField(max_length=100, unique=True, blank=False, null=True)
+    title = models.CharField(max_length=200, unique=True, blank=False, null=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=False, null=True)
 
     def get_absolute_url(self):
         return reverse('category', args=(self.slug, ))
@@ -33,14 +33,14 @@ class Tag(models.Model):
 class Post(models.Model):
     # Relations
     author = models.ForeignKey('auth.User')
-    category = models.ManyToManyField(Category, blank=True, through='CategoryToPost')
-    tag = models.ManyToManyField(Tag, blank=True, through='TagToPost')
+    category = models.ForeignKey(Category, blank=False, null=True)
+    tag = models.ManyToManyField(Tag, blank=False, through='TagToPost')
 
     # Attributes
     title = models.CharField(max_length=200, unique=True, blank=False)
     slug = models.SlugField(max_length=100, unique=True, blank=False, null=True)
     description = MartorField(default='')
-    text = MartorField()
+    text = MartorField(default='')
     created_date = models.DateTimeField(default=timezone.now, blank=False, null=True)
     published_date = models.DateTimeField(blank=False, null=True)
     preview_image = models.ImageField(upload_to="image/", blank=True, null=True)
@@ -52,9 +52,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', args=(self.slug, ))
-
-    def categories(self):
-        return '\n'.join(cat.title for cat in self.category.all())
 
     def tags(self):
         return ', '.join(tg.title for tg in self.tag.all())
@@ -68,15 +65,6 @@ class Post(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         ordering = ['-id']
-
-
-class CategoryToPost(models.Model):
-    post = models.ForeignKey(Post)
-    category = models.ForeignKey(Category)
-
-    class Meta:
-        verbose_name = 'Category to post'
-        verbose_name_plural = 'Categories to post'
 
 
 class TagToPost(models.Model):
