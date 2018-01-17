2fbeaae7bcc53217804623cf5c769d39e144c351
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-17 12:45:28 +0200

    Update url for feed.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^categories/?$', CategoryListView.as_view(), name='categories'),
     url(r'^category/(?P<slug>[-\w\d\_]+)/$', CategoryView.as_view(),
         name='category'),
-    url(r'^feeds/$', PostsFeed()),
+    url(r'^feeds/atom.xml$', PostsFeed.as_view(), name='rss_feed'),
     url(r'^(?P<slug>[-\w\d\_]+)/$', PostView.as_view(), name='post'),
 ]
