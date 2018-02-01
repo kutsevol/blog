08abf7cf01e48fc9e4473a97326baf27f31c8877
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-01 19:33:33 +0200

    Add comments into code. Add TODO

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,15 @@ from .views import PostListView, PostView, CategoryView, CategoryListView, \
     PostsFeed
 
 urlpatterns = [
+    # index page
     url(r'^$', PostListView.as_view(), name='posts'),
+    # page with all categories
     url(r'^categories/?$', CategoryListView.as_view(), name='categories'),
+    # TODO need check this url, maybe need remove its
     url(r'^category/(?P<slug>[-\w\d\_]+)/$', CategoryView.as_view(),
         name='category'),
+    # feed rss page
     url(r'^feeds/atom.xml$', PostsFeed.as_view(), name='rss_feed'),
+    # each post page
     url(r'^(?P<slug>[-\w\d\_]+)/$', PostView.as_view(), name='post'),
 ]
