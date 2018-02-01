de2a648f801e10958f74d6b2ffd75981f9cceb8a
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-01 19:27:32 +0200

    Add docstring.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,4 +3,10 @@ from django.template.defaulttags import register
 
 @register.filter
 def get_item(dictionary, key):
+    """
+    Register own template filter.
+    :param dictionary: expected dict object
+    :param key: expected unmutable type, key for dict
+    :return: key from dict or None
+    """
     return dictionary.get(key)
