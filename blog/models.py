35d4c138c325847052d0e90ca63d045568258baa
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-06 00:19:53 +0200

    Small refactoring

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from martor.models import MartorField
@@ -105,7 +106,7 @@ class Post(models.Model):
     and file name;
     null - If True, Django will store empty values as NULL in the database.
     """
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(User)
     category = models.ForeignKey(Category, default='')
     tag = models.ManyToManyField(Tag, blank=False, through='TagToPost')
 
@@ -113,7 +114,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=100, unique=True, default='')
     description = MartorField(default='', blank=False)
     text = MartorField(default='', blank=False)
-    created_date = models.DateTimeField(default=timezone.now, blank=False,
+    created_date = models.DateTimeField(auto_now_add=True, blank=False,
                                         null=True)
     published_date = models.DateTimeField(blank=False, null=True)
     preview_image = models.ImageField(upload_to="image/", blank=True,
